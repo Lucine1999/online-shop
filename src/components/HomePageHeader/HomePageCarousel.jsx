b665e7bbc27d5f1e965e851a807d4a8c514c2c57
Hustleea <43501936+Hustleea@users.noreply.github.com>
2022-03-30 22:41:00 +0400

    corousel styles fixed

--- src/components/HomePageHeader/HomePageCarousel.jsx
+++ src/components/HomePageHeader/HomePageCarousel.jsx
@@ -5,18 +5,19 @@ import image1 from '../../assets/image1.jpg'
 import image2 from '../../assets/image2.jpg'
 import image3 from '../../assets/image3.jpg'
 
+const images = [image1, image2, image3]
+
 const Homepagecarusel = () => {
     return (
         <Carousel width="100%" className='main-slide' autoPlay dynamicHeight emulateTouch infiniteLoop showThumbs={false}>
-            <div>
-                <img Height='600px' src={image1} alt={image1} />
-            </div>
-            <div>
-                <img Height='600px' src={image2} alt={image2}/>
-            </div>
-            <div>
-                <img Height='600px' src={image3} alt={image3}/>
-            </div>
+            {
+                images.map(el => {
+                return (
+                <div key={el}>
+                    <img style={{maxHeight: '800px'}} src={el} alt={el} />
+                </div>
+                )})
+            }
         </Carousel>
     );
 }

bae52fcd2f6501c1b212bfa288cb2da79aa023b9
srapionyanartur <arthur.srapionyan@gmail.com>
2022-04-01 13:57:14 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
-
+import Login from './components/login';
+import Signup from './components/signup';
 function App() {
   return (
     <div className="App">
+      <Login />
+      <Signup/>
     </div>
   );
 }
+
 
 export default App;

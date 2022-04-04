ca8beb10e3dd536e1696b04683108e7dbe2ae340
Lucine1999 <lusine.mardan@gmail.com>
2022-04-04 15:14:10 +0400

    changes

--- src/pages/cartPage/CartProducts.jsx
+++ src/pages/cartPage/CartProducts.jsx
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Container from "@material-ui/core/Container";
+// import Container from "@material-ui/core/Container";
 
 import InputBase from "@mui/material/InputBase";
 import IconButton from "@mui/material/IconButton";

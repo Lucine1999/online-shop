352c8cd0da50dbe8b34136310acd545e7bda484c
Hustleea <43501936+Hustleea@users.noreply.github.com>
2022-04-04 21:45:10 +0400

    npm install -g eslint

--- src/components/main-product-List/ListItem.jsx
+++ src/components/main-product-List/ListItem.jsx
@@ -2,16 +2,34 @@ import BoxComponent from "../main-product-List/BoxComponent";
 import PaginationRounded from "../main-product-List/pagination";
 import {useSelector} from "react-redux";
 import { selectProducts } from "../../features/products/productsSlice";
+import { useEffect, useState } from "react";
+import { useLocation } from 'react-router-dom'
+
 
 function ListItem() {
+    const location = useLocation();
     const products = useSelector(selectProducts);
+    const [previusPage, setPreviusPage] = useState(parseInt(location.search?.split('=')[1] - 1 || 0));
+    const [currentPage, setCurrentPage] = useState(parseInt(location.search?.split('=')[1] || 1));
+
+    const productCount = 1;
+
+    useEffect(() => {
+        setPreviusPage(currentPage -1)
+      }, [currentPage])
+
+
     return (
         <main className="product-list">
             
             <div className="items">
-                {products ? products.map((value) => BoxComponent(value)) : null}
+                {products ? products.map((value, idx) => {
+                    if (idx < currentPage * productCount && idx >= previusPage * productCount) {
+                        return BoxComponent(value);
+                    }
+                }) : null}
             </div>
-            <PaginationRounded />
+            <PaginationRounded page={products} productCount={productCount} setCurrentPage={setCurrentPage} currentPage={currentPage} />
         </main>
     );
 }

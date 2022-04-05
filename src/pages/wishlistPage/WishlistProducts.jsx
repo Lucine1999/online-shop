28d40a7ba197b6d0f106acd07d068a0299e7d4c1
srapionyanartur <arthur.srapionyan@gmail.com>
2022-04-05 23:24:56 +0400

    second commit-corrected

--- src/pages/wishlistPage/WishlistProducts.jsx
+++ src/pages/wishlistPage/WishlistProducts.jsx
@@ -6,7 +6,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
-import Button from "mui-button";
+import Button from '@mui/material/Button';
 import DeleteIcon from "@mui/icons-material/Delete";
 
 import cart_img1 from "../../assets/cart_img1.jpg";

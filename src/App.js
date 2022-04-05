c4a184724b296f2a6932482eaa0bcbad2433065f
srapionyanartur <arthur.srapionyan@gmail.com>
2022-04-05 22:19:27 +0400

    wishlist first commit

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import { auth, onAuthStateChanged, db, collection, getDocs } from "./firebase";
 import LogIn from "./pages/LogIn";
 import SignUp from "./pages/SignUp";
 import CartPage from "./pages/cartPage/CartPage";
+import WishlistPage from "./pages/wishlistPage/WishlistPage";
 import NotFound from "./pages/NotFound";
 import { getWishlistItems } from "./features/products/productsSlice";
 import "./App.css";
@@ -72,6 +73,8 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/favorites" element={<WishlistPage />} />
+
       </Routes>
       <Footer />
     </>
